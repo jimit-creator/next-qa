4023ea3f7a6b2f2ba0389a3d11197badd3252d0c
Jimit <jimit@brightforce.tech>
2025-06-09 09:30:59 +0530

    - Design changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,6 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/lib/auth';
-import { NextRequest } from 'next/server';
 
-const handler = (req: NextRequest, context: { params: { nextauth: string[] } }) => {
-  return NextAuth(req, context, authOptions);
-};
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
